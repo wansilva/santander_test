b5f91c63aa34d25ae9fce49d5e2ff1a839727a2f
Wanderson Silva <wansilvasan@gmail.com>
2023-09-25 10:54:51 -0300

    fix login
    Change the post login route to the users page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
 
   sendLogin() {
     if (this.loginForm.valid) {
-      this.router.navigate(["/home"]);
+      this.router.navigate(["/users"]);
     } else {
     }
   }
